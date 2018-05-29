954fad9cc82a0dc0164b4c6654f9f5b9233e70e9
wales.yu <wales520@gmail.com>
2018-05-29 22:28:06 +0800

    完成modal 警告

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
-    <transition name="modal"  v-if="show_modal">
-        <div class="modal" style="display: block">
+    <transition name="modal" v-if="show_modal">
+        <div class="modal" style="display: block" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -9,10 +9,11 @@
                         </h4>
                     </div>
                     <div class="modal-body">
-                       {{content}}
+                        <icon name="user-md" scale="2"/>&nbsp;
+                        {{content}}
                     </div>
                     <div class="modal-footer">
-                      <button type="button" class="btn btn-outline-info" @click="closeModal()"> Close </button>
+                        <button type="button" class="btn btn-outline-info" @click="closeModal"> 關閉 </button>
                     </div>
                 </div>
             </div>
@@ -21,76 +22,33 @@
 </template>
 
 <script>
-export default {
-   name: 'Modal',
-   props: ["title", "content","showModal"],
-  data () {
-    return { 
-      show_modal : this.showModal
-    } 
-  },
-  methods: {
-    closeModal: function(){
-        this.show_modal = false;
+    import "vue-awesome/icons";
+    import Icon from "vue-awesome/components/Icon";
+    export default {
+        name: 'Modal',
+        components:{
+            Icon
+        },
+        props: ["title", "content", "showModal"],
+        data: function () {
+            return {
+                show_modal: this.showModal
+            }
+        },
+        watch:{
+            showModal:function(newVal, oldVal){
+                this.show_modal = newVal;
+            }
+        },
+        methods: {
+            closeModal: function () {
+                this.$emit("childCloseModal", false);
+                this.show_modal = false;
+            }
+        }
     }
-  }
-}
 </script>
 
 <style scoped>
-  .modal-mask {
-    position: fixed;
-    z-index: 9998;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: table;
-    transition: opacity 0.3s ease;
-  }
 
-  .modal-wrapper {
-    display: table-cell;
-    vertical-align: middle;
-  }
-
-  .modal-container {
-    width: 300px;
-    margin: 0px auto;
-    padding: 20px 30px;
-    background-color: #fff;
-    border-radius: 2px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-    transition: all 0.3s ease;
-    font-family: Helvetica, Arial, sans-serif;
-  }
-
-  .modal-header h3 {
-    margin-top: 0;
-    color: #42b983;
-  }
-
-  .modal-body {
-    margin: 20px 0;
-  }
-
-  .modal-default-button {
-    float: right;
-  }
-
-  .modal-enter {
-    opacity: 0;
-  }
-
-  .modal-leave-active {
-    opacity: 0;
-  }
-
-  .modal-enter .modal-container,
-  .modal-leave-active .modal-container {
-    -webkit-transform: scale(1.1);
-    transform: scale(1.1);
-  }
-  
 </style>
--- src/components/Prescriptions.vue
+++ src/components/Prescriptions.vue
@@ -37,13 +37,14 @@
                         <button type="button" class="btn btn-info" @click="addItem">增加這一味藥</button>
                     </div>
                     <div class="form-group">
+                        <label>藥單名稱 :</label>
+                        <input type="text" v-model="presctiption_title" />
                         <label>每一行筆數 :</label>
                         <select v-model="item_perline" class="form-control">
-                            <option v-for="item in 5" :value="item +1 ">{{item +1}}</option>
+                            <option v-for="item in 5" :value="item +1 " v-bind:key="item">{{item +1}}</option>
                         </select>
                     </div>
                     <div class="form-group">
-                        <label>&nbsp;</label>
                         <button type="button" @click="print" class='btn btn-primary'>印啦印啦</button>
                     </div>
                 </form>
@@ -52,98 +53,92 @@
         <div class="row">
             <div class="col-sm-11 result_area pull-right">
                 <span v-for="(row,index) in rows" v-bind:key="row.id">
-                    {{row['material']}}
+                    <span>{{row['material']}}</span>
                     <span class='value'>{{row['dose']}}</span>
                     <span class='unit'> {{row['dose_unit']}}</span>&nbsp;&nbsp;
                     <br v-if="index % item_perline == item_perline-1">
                 </span>
             </div>
         </div>
-        <Modal :title="modal_title" :content="modal_content" :showModal="showModal">
-        </Modal>
+        <Modal :title="modal_title" :content="modal_content" :showModal="showModal" @childCloseModal="close_modal" />
     </div>
 </template>
 
 <script>
-import Modal from './Modal'
+    import Modal from './Modal'
+    export default {
+        name: 'Prescriptions',
+        components: {
+            Modal
+        },
+        data: function () {
+            return {
+                rows: [],
+                presctiption_title: 'OO中藥行用簽',
+                modal_title: '注意',
+                modal_content: '',
+                show_tools: true,
+                showModal: false,
+                dose: 1,
+                item_perline: 4,
+                material: '',
+                dose_unit: '錢',
+                units: [{
+                    id: 1,
+                    value: '錢'
+                },
+                {
+                    id: 2,
+                    value: '兩'
+                },
+                {
+                    id: 3,
+                    value: '斤'
+                },
+                {
+                    id: 4,
+                    value: '片'
+                },
+                {
+                    id: 5,
+                    value: '枚'
+                }
+                ]
+            };
+        },
+        methods: {
+            addItem: function (params) {
+                if (this.material == "" || this.dose == "" || this.dose_unit == "") {
+                    this.showModal = true;
+                    this.modal_content = '沒有填入藥材名稱！';
+                    return false;
+                }
 
-export default {
-  name: 'Prescriptions',
-  components:{
-    Modal
-  },
-  data () {
-    return {
-      rows: [],
-      presctiption_title: 'OO中藥行用簽',
-      modal_title:'注意',
-      modal_content:'',
-      show_tools : true,
-      showModal: false ,
-      dose : 1,
-      item_perline:4,
-      material : '',
-      dose_unit:'錢',
-      units: [
-        {
-          id: 1,
-          value: '錢'
-        },
-        {
-          id: 2,
-          value: '兩'
-        },
-        {
-          id: 3,
-          value: '斤'
-        },
-        {
-          id: 4,
-          value: '片'
-        },
-        {
-          id: 5,
-          value: '枚'
+                let data = {
+                    material: this.material,
+                    dose: this.dose,
+                    dose_unit: this.dose_unit
+                }
+                this.rows.push(data);
+                this.material = '';
+                this.dose = 1;
+                this.dose_unit = '錢';
+            },
+            print: function () {
+                let that = this;
+                this.show_tools = false;
+                setTimeout(function () {
+                    window.print();
+                }, 10);
+                setTimeout(function () {
+                    that.show_tools = true;
+                }, 1000);
+            },
+            close_modal: function () {
+                this.showModal = false;
+            }
         }
-      ]
-    };
-  },
-  methods: {
-    addItem: function(params) {
-        if(this.material =="" || this.dose=="" || this.dose_unit==""){
-            this.showModal = true;
-            Modal.showModal = true;
-            this.modal_content = "沒有填入藥材名稱！";
-            return false;
-        }
-        let data = {
-            material : this.material,
-            dose : this.dose,
-            dose_unit : this.dose_unit
-        }
-        this.rows.push(data);
-        this.material = '';
-        this.dose = 1;
-        this.dose_unit = '錢';
-    },
-    print: function(){
-        let that = this;
-        this.show_tools = false;
-        
-        setTimeout(function() {
-             window.print();
-        }, 10);
-        
-        setTimeout(function() {
-            that.show_tools = true;
-        }, 1000);
-    },    
-    closeModal: function(p){
-        console.log(111);
-        this.showModal = false;
     }
-  }
-}
 </script>
 
 <style scoped>
@@ -152,22 +147,22 @@ export default {
         font-size: 18pt;
         font-family: 標楷體;
         margin: 10px;
-  }
+    }
 
-  .unit {
+    .unit {
         position: relative;
         left: 10px;
         font-size: 12pt;
         font-family: 標楷體;
-  }
+    }
 
-  .value {
+    .value {
         position: relative;
         left: 10px;
         writing-mode: lr;
         font-size: 12pt;
         font-family: 細明體;
-  }
+    }
 
     #div_title {
         font-size: 18pt;
@@ -175,11 +170,11 @@ export default {
         display: none;
     }
 
-    .presctiption_title{
+    .presctiption_title {
         margin: 0 auto;
         width: 220px;
         color: red;
         font-family: 標楷體;
-        font-size:20pt;
+        font-size: 20pt;
     }
 </style>
